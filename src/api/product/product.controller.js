6d9fa930bddde27b604727535671824520f04577
Rushil-2311 <rushilpatellinux2gmail.com>
2022-09-14 23:49:20 +0530

    json-directory-name-not-getting-issue-fixed

--- src/api/product/product.controller.js
+++ src/api/product/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../../model/product");
 const products = require("../json/product.json");
+var path = require("path");
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
 module.exports.addProduct = (req, res) => {
@@ -29,24 +30,22 @@ module.exports.createProduct = (req, res) => {
     url,
     id: uuid(),
   };
-  fs.readFile(
-    "/home/rushil/Desktop/node-test/src/api/json/product.json",
-    "utf8",
-    function (err, data) {
-      var obj = JSON.parse(data);
-      console.log(obj, "hey");
-      obj.push(PorductData);
-      var strNotes = JSON.stringify(obj);
-      fs.writeFile(
-        "/home/rushil/Desktop/node-test/src/api/json/product.json",
-        strNotes,
-        function (err) {
-          if (err) return res.status(400).json({ err });
-          res.status(200).json({ obj });
-        }
-      );
-    }
-  );
+  var jsonPath = path.join(__dirname, "..", "json", "product.json");
+  fs.readFile(jsonPath, "utf8", function (err, data) {
+    var obj = JSON.parse(data);
+    obj.push(PorductData);
+    var strNotes = JSON.stringify(obj);
+    fs.writeFile(jsonPath, strNotes, function (err) {
+      if (err) return res.status(400).json({ err });
+      res.status(200).json({ obj });
+    });
+  });
+};
+
+module.exports.readJsonFile = (req, res) => {
+  const data = fs.readFileSync(jsonPath);
+  var obj = JSON.parse(data);
+  console.log(obj, "obj");
 };
 
 module.exports.getAllproduct = (req, res) => {
